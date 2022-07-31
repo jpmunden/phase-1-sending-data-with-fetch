124f550bf5cdf330ccd601b1f32ca6d1a7f0940c
Jacob <jacobmunden@gmail.com>
2022-07-31 16:38:55 -0600

    commit tree 4d679d5a8bf3366423e2e7dde5d10ea854222bdd

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
 // Add your code here
+/*
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+
+fetch('http://localhost:3000/dogs/', configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    })
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+    });
+    
+    .catch(error => {
+        if (error.response) {
+            console.log('Unauthorized Access')
+*/
+
+/*
+function submitData(firstName, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Accept": "application/json",
+        },
+        body:JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    })
+    .then(res => res.json())
+    .then(console.log(firstName, email))   
+};
+*/
+
+
+
+
+function submitData(firstName, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            'name': firstName,
+            'email': email
+         }),
+     })
+        .then(res => {
+            return res.json()})
+        .then(getID)
+        .catch(errorMessage)
+};
+
+function getID(object) {
+    document.body.innerHTML = object.id
+}
+
+function errorMessage() {
+    document.body.innerHTML = "Unauthorized Access"
+}
